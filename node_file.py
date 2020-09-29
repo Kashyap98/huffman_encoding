1beba9044dd1e080b3aca34498e1c2ea669fbc3f
Kashyap Patel <k.patel1098@gmail.com>
2020-09-29 11:54:36 -0500

    init commit

---
+++ node_file.py
@@ -0,0 +1,8 @@
+class Node(object):
+
+    def __init__(self, value=None, letter=None, left=None, right=None):
+        """ A typical binary tree node """
+        self.value = value
+        self.letter = letter
+        self.left = left
+        self.right = right
